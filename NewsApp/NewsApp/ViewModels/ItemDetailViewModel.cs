6d7a705ff92452ffd4bc5db1386f9dae777310a4
msafarigit <msafari.int@gmail.com>
2018-12-12 21:07:25 +0330

    Add project files.

---
+++ NewsApp/NewsApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NewsApp.Models;
+
+namespace NewsApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
